81a59f04991da40a0263fad3b703fd8a1e98ba38
bseifert <none@none>
2003-11-26 20:04:12 +0000

    Moved platform.x from setup to common.

---
+++ common/platform.cpp
@@ -0,0 +1,43 @@
+// platform.cpp	- Implementation of PlatformIsWindows9x()
+//
+// WinDirStat - Directory Statistics
+// Copyright (C) 2003 Bernhard Seifert
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// Author: bseifert@users.sourceforge.net, bseifert@daccord.net
+
+#ifndef WINVER				// Allow use of features specific to Windows 95 and Windows NT 4 or later.
+#define WINVER 0x0400		// Change this to the appropriate value to target Windows 98 and Windows 2000 or later.
+#endif
+
+#include <afxwin.h>         // MFC core and standard components
+#include "platform.h"
+
+bool PlatformIsWindows9x()
+{
+	OSVERSIONINFO osvi;
+	ZeroMemory(&osvi, sizeof(osvi));
+	osvi.dwOSVersionInfoSize= sizeof(osvi);
+
+	if (!GetVersionEx(&osvi))
+	{
+		TRACE("GetVersionEx() failed.\r\n");
+		return false;
+	}
+
+	return (osvi.dwPlatformId == VER_PLATFORM_WIN32_WINDOWS);
+}
+
