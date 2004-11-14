e19e2c46e928d0a6c15d13ceba2dd31044ba8a65
assarbad <none@none>
2004-11-14 21:55:12 +0000

    - Adapted to new folder structure (i.e. removed blank spaces) for cleanup

--- cleanbuild.cmd
+++ cleanbuild.cmd
@@ -15,8 +15,8 @@ del /f /q /s *.aps
 del /f /q .\common\linkcounter.exe
 :: rem Delete all release subdirectories (the release of the binaries is not touched)
 for /d %%i in (*) do @(
-  rd /s /q ".\%%i\Unicode Release"
-  rd /s /q ".\%%i\Unicode Debug"
+  rd /s /q ".\%%i\UnicodeRelease"
+  rd /s /q ".\%%i\UnicodeDebug"
   rd /s /q ".\%%i\Release"
   rd /s /q ".\%%i\Debug"
 )
