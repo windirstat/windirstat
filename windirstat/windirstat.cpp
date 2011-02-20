df82bdeaf1b597a080fc3cdb597b571048e2f3ff
Oliver Schneider <oliver@assarbad.net>
2011-02-20 02:14:33 +0000

    - Use modern common controls

--- windirstat/windirstat.cpp
+++ windirstat/windirstat.cpp
@@ -38,6 +38,9 @@
 #define new DEBUG_NEW
 #endif
 
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' "\
+    "version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 class CMiniDumper
 {
 private:
