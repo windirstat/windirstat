f6e1ff7132e99b2771101caf8d180bf458db938c
Oliver Schneider <oliver@assarbad.net>
2014-01-03 05:29:23 +0000

    Improvements to premake4.lua

--- premake4.lua
+++ premake4.lua
@@ -73,7 +73,7 @@ do
         end
     end]]
 end
-local function transformMacroNames(action, input)
+local function transformMN(input)
     local new_map   = { vs2010 = 0, vs2012 = 0, vs2013 = 0 }
     local replacements = { PlatformName = "Platform", ConfigurationName = "Configuration" }
     if new_map[action] ~= nil then
@@ -92,7 +92,7 @@ solution ("windirstat")
     location        ('.')
 
     project ("windirstat")
-        local int_dir   = transformMacroNames(action, "intermediate/" .. action .. "_" .. "$(PlatformName)_$(ConfigurationName)")
+        local int_dir   = "intermediate/" .. action .. "_$(" .. transformMN("PlatformName") .. ")_$(" .. transformMN("ConfigurationName") .. ")"
         uuid            ("BD11B94C-6594-4477-9FDF-2E24447D1F14")
         language        ("C++")
         kind            ("WindowedApp")
@@ -200,7 +200,7 @@ solution ("windirstat")
             for nm,guid in pairs(resource_dlls) do
                 premake.CurrentContainer = oldcurr
                 prj = project(nm)
-                    local int_dir   = transformMacroNames(action, "intermediate/" .. action .. "_" .. "$(PlatformName)_$(ConfigurationName)")
+                    local int_dir   = "intermediate/" .. action .. "_$(" .. transformMN("PlatformName") .. ")_$(" .. transformMN("ConfigurationName") .. ")"
                     uuid            (guid)
                     language        ("C++")
                     kind            ("WindowedApp")
