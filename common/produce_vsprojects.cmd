4023312f2cfcfd07a395f1a5060af9244b6b7667
Oliver Schneider <oliver@assarbad.net>
2014-01-20 04:34:16 +0000

    Newest development project Adjusted the script which creates the projects based on premake4.lua

--- common/produce_vsprojects.cmd
+++ common/produce_vsprojects.cmd
@@ -13,13 +13,16 @@ set REPOROOT=%~dp0..
 set COMMON=%~dp0
 :: Change into the repository root
 setlocal & pushd "%REPOROOT%"
-if "%~1" == "full"      shift&set OPTIONS=--resources --sdk71
-if "%~1" == "sdk71"     shift&set OPTIONS=--sdk71
-if "%~1" == "resources" shift&set OPTIONS=--resources
+if "%~1" == "full"      set OPTIONS=--resources --sdk71
+if "%~1" == "sdk71"     set OPTIONS=--sdk71
+if "%~1" == "resources" set OPTIONS=--resources
+set DEFAULT_VSVERSIONS=vs2005 vs2008 vs2010 vs2012 vs2013
 set VSVERSIONS=%*
-if "%VSVERSIONS%" == "" set VSVERSIONS=vs2005 vs2008 vs2010 vs2012 vs2013
+if "%VSVERSIONS%" == "" set VSVERSIONS=%DEFAULT_VSVERSIONS%
 for %%i in (%VSVERSIONS%) do @(
-  "%COMMON%\premake4.exe" %OPTIONS% %%i
+  for %%j in (%DEFAULT_VSVERSIONS%) do @(
+    if "%%i" == "%%j" "%COMMON%\premake4.exe" %OPTIONS% %%i
+  )
 )
 :: Back to normal
 popd & endlocal
