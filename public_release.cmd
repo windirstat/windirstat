96bb7ba512b87776e6ccd70e7be4828df1266735
Oliver Schneider <oliver@assarbad.net>
2014-01-20 00:58:52 +0000

    This may need further adjustment, will probably have a completely separate solution for releases

--- public_release.cmd
+++ public_release.cmd
@@ -1,8 +1,11 @@
 @echo off
 setlocal ENABLEEXTENSIONS & pushd .
-call %~dp0common\setvcvars.cmd 8.0
+call %~dp0common\setvcvars.cmd 2005
+call %~dp0common\buildinc.cmd "%~dp0common"
 echo %VCVER_FRIENDLY%
-vcbuild.exe /time /rebuild /nologo "/htmllog:$(SolutionDir)\buildlog32.html" "%~dp0\windirstat.vs2005.sln" "Release|Win32"
-vcbuild.exe /time /rebuild /nologo "/htmllog:$(SolutionDir)\buildlog64.html" "%~dp0\windirstat.vs2005.sln" "Release|x64"
+vcbuild.exe /time /rebuild /showenv /M4 /nologo ^
+    "/htmllog:$(SolutionDir)build\buildlog32.html" "%~dp0\windirstat.vs8.sln" "Release|Win32"
+vcbuild.exe /time /rebuild /showenv /M4 /nologo ^
+    "/htmllog:$(SolutionDir)build\buildlog64.html" "%~dp0\windirstat.vs8.sln" "Release|x64"
 popd & endlocal
 goto :EOF
