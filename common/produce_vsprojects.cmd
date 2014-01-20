6d3fd53244706c98575d5e7b2169556d8ae4cdcc
Oliver Schneider <oliver@assarbad.net>
2014-01-20 00:03:16 +0000

    Enable creation of projects/solutions for particular versions of Visual Studio only

--- common/produce_vsprojects.cmd
+++ common/produce_vsprojects.cmd
@@ -1,7 +1,7 @@
 @echo off
 @if not "%OS%"=="Windows_NT" @(echo This script requires Windows NT 4.0 or later to run properly! & goto :EOF)
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-::: 2013, Oliver Schneider (assarbad.net) - PUBLIC DOMAIN/CC0
+::: 2013, 2014, Oliver Schneider (assarbad.net) - PUBLIC DOMAIN/CC0
 :::
 ::: DISCLAIMER: Disclaimer: This software is provided 'as-is', without any
 :::             express or implied warranty. In no event will the author be
@@ -13,10 +13,12 @@ set REPOROOT=%~dp0..
 set COMMON=%~dp0
 :: Change into the repository root
 setlocal & pushd "%REPOROOT%"
-if "%~1" == "full"      set OPTIONS=--resources --sdk71
-if "%~1" == "sdk71"     set OPTIONS=--sdk71
-if "%~1" == "resources" set OPTIONS=--resources
-for %%i in (vs2005 vs2008 vs2010 vs2012 vs2013) do @(
+if "%~1" == "full"      shift&set OPTIONS=--resources --sdk71
+if "%~1" == "sdk71"     shift&set OPTIONS=--sdk71
+if "%~1" == "resources" shift&set OPTIONS=--resources
+set VSVERSIONS=%*
+if "%VSVERSIONS%" == "" set VSVERSIONS=vs2005 vs2008 vs2010 vs2012 vs2013
+for %%i in (%VSVERSIONS%) do @(
   "%COMMON%\premake4.exe" %OPTIONS% %%i
 )
 :: Back to normal
