a619ae36ccb768053f79dbbf4eed6828ca888ea0
Bryan Berns <berns@uwalumni.com>
2024-04-16 23:19:08 -0400

    Remove CHM Help / Setup Installer
    - Removed legacy help file data since we will be deferring to online help.
    - Adjusted Microsoft installer based on recent changes and created a simple command script to run the Wix 3.x toolset.  This should be adjusted to use Wix 4.x (or another installer platform) in the future.
    - Removed older, executable-based legacy installer artifacts.

---
+++ setup/build.cmd
@@ -0,0 +1,18 @@
+@ECHO OFF
+TITLE Building WinDirStat Installer
+
+:: setup environment variables based on location of this script
+CD /D "%~dp0"
+SET PX86=%PROGRAMFILES(X86)%
+SET BLDDIR=..\build
+FOR /F "DELIMS=" %%X IN ('DIR "%PX86%\WiX Toolset*" /B /AD') DO SET PATH=%PATH%;%PX86%\%%~nxX\bin
+
+:: create the installers
+candle -arch x86 "WinDirStat.wxs" -o "WinDirStat-x86.wixobj"
+light -ext WixUIExtension -ext WixUtilExtension -sval "WinDirStat-x86.wixobj" -o "%BLDDIR%\WinDirStat-x84.msi"
+candle -arch x64 "WinDirStat.wxs" -o "WinDirStat-x64.wixobj"
+light -ext WixUIExtension -ext WixUtilExtension -sval "WinDirStat-x64.wixobj" -o "%BLDDIR%\WinDirStat-x64.msi"
+DEL /F "*.wixobj"
+DEL /F "%BLDDIR%\*.wixpdb"
+
+EXIT /B 0
