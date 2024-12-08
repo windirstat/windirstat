7bd3aaa79f63cfb36503e012b71c34988a0ed44d
Bryan Berns <berns@uwalumni.com>
2024-12-08 12:04:38 -0500

    Chocolatey Packager Tweaks

--- setup/chocolatey/tools/chocolateyinstall.ps1
+++ setup/chocolatey/tools/chocolateyinstall.ps1
@@ -3,7 +3,8 @@
 $packageName= $env:ChocolateyPackageName
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $metadata = Get-Content "${PSScriptRoot}\chocolateymetadata.json" | ConvertFrom-Json
-$url = 'https://github.com/windirstat/windirstat/releases/download/release/'
+
+$url = 'https://github.com/windirstat/windirstat/releases/download/release/v'
 $urlx86 = $url + $Metadata.version + '/WinDirStat-x86.msi'
 $urlx64 = $url + $Metadata.version + '/WinDirStat-x64.msi'
 
@@ -16,9 +17,9 @@ $packageArgs = @{
 
   softwareName  = 'WinDirStat'
 
-  checksum      = $metadata.hashX64
+  checksum      = $metadata.hashX86
   checksumType  = 'sha256'
-  checksum64    = $metadata.hashX86
+  checksum64    = $metadata.hashX64
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
