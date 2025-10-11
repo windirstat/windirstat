2e58048528c9eacfd5108e8c2ef738c8b53a49b1
Bryan Berns <berns@uwalumni.com>
2025-10-11 12:06:02 -0400

    Fix Last Column Header Autosize Behavior

--- windirstat/Controls/OwnerDrawnListControl.cpp
+++ windirstat/Controls/OwnerDrawnListControl.cpp
@@ -592,10 +592,14 @@ void COwnerDrawnListControl::OnHdnDividerdblclick(NMHDR* pNMHDR, LRESULT* pResul
     const int column = reinterpret_cast<LPNMHEADER>(pNMHDR)->iItem;
     const int subitem = ColumnToSubItem(column);
 
-    // fetch size of rendered column header
+    // fetch size of rendered column header text
+    // temporarily insert a false column to the finalize column does 
+    // not autosize to fit the whole control width
     SetRedraw(FALSE);
+    const int falseColumn = InsertColumn(column + 1, L"");
     SetColumnWidth(column, LVSCW_AUTOSIZE_USEHEADER);
     int width = GetColumnWidth(column);
+    DeleteColumn(falseColumn);
 
     // fetch size of sub-elements
     for (int i = 0, itemMax = GetItemCount(); i < itemMax; i++)
