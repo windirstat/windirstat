1d5ea6f90ca7b1ee121b64b8910b656df7a10bf7
Bryan Berns <berns@uwalumni.com>
2026-01-06 13:09:57 -0500

    Insert False Column At End For Autosize

--- windirstat/Controls/OwnerDrawnListControl.cpp
+++ windirstat/Controls/OwnerDrawnListControl.cpp
@@ -775,7 +775,7 @@ void COwnerDrawnListControl::OnHdnDividerdblclick(NMHDR* pNMHDR, LRESULT* pResul
     // temporarily insert a false column to the finalize column does
     // not autosize to fit the whole control width
     SetRedraw(FALSE);
-    const int falseColumn = InsertColumn(column + 1, L"");
+    const int falseColumn = InsertColumn(GetHeaderCtrl()->GetItemCount() + 1, L"");
     SetColumnWidth(column, LVSCW_AUTOSIZE_USEHEADER);
     int width = GetColumnWidth(column);
     DeleteColumn(falseColumn);
