6ee06e4d65cf0b784edca1a4389a655e10d0541c
Bryan Berns <berns@uwalumni.com>
2026-01-25 14:50:22 -0500

    Version Bump

--- windirstat/Version.h
+++ windirstat/Version.h
@@ -35,7 +35,7 @@
 
 #define PRD_MAJVER                  2 // major product version
 #define PRD_MINVER                  5 // minor product version
-#define PRD_PATCH                   0 // patch number for product
+#define PRD_PATCH                   1 // patch number for product
 #define PRD_BUILD                   GIT_COUNT // build number for product
 #define FILE_MAJVER                 PRD_MAJVER // major file version
 #define FILE_MINVER                 PRD_MINVER // minor file version
