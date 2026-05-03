789f5c3ceba17a3d41312e181674b86eb1393c26
harryytm <harryytm@gmail.com>
2026-05-03 14:36:04 +0100

    Change Icons
    ▼ for Largest Files
    ⫘ for Hardlinks
    ⧉ for Duplicated files / Hardlinks referenced data size
    ⌕ for Search Results

--- windirstat/Controls/IconHandler.cpp
+++ windirstat/Controls/IconHandler.cpp
@@ -34,10 +34,10 @@ void CIconHandler::Initialize()
         m_junctionProtected = IconFromFontChar(L'⤷', RGB(0xFF, 0x3A, 0x3A), true);
         m_freeSpaceImage = IconFromFontChar(L'▢', RGB(0x3A, 0xCC, 0x3A), true);
         m_emptyImage = IconFromFontChar(L'▢', DarkMode::WdsSysColor(COLOR_WINDOWTEXT));
-        m_hardlinksImage = IconFromFontChar(L'⧉', DarkMode::WdsSysColor(COLOR_WINDOWTEXT));
-        m_dupesImage = IconFromFontChar(L'⧈', DarkMode::WdsSysColor(COLOR_WINDOWTEXT));
-        m_searchImage = IconFromFontChar(L'⊙', DarkMode::WdsSysColor(COLOR_WINDOWTEXT));
-        m_largestImage = IconFromFontChar(L'⋙', DarkMode::WdsSysColor(COLOR_WINDOWTEXT));
+        m_hardlinksImage = IconFromFontChar(L'⫘', DarkMode::WdsSysColor(COLOR_WINDOWTEXT));
+        m_dupesImage = IconFromFontChar(L'⧉', DarkMode::WdsSysColor(COLOR_WINDOWTEXT));
+        m_searchImage = IconFromFontChar(L'⌕', DarkMode::WdsSysColor(COLOR_WINDOWTEXT));
+        m_largestImage = IconFromFontChar(L'▼', DarkMode::WdsSysColor(COLOR_WINDOWTEXT));
         m_unknownImage = IconFromFontChar(L'?', RGB(0xCC,0xB8,0x66), true);
         m_defaultFileImage = FetchShellIcon(GetSysDirectory() + L"\\~", 0, FILE_ATTRIBUTE_NORMAL);
         m_defaultFolderImage = FetchShellIcon(GetSysDirectory() + L"\\~", 0, FILE_ATTRIBUTE_DIRECTORY);
